f83b4c80cb3ec38b0766909e0da4e9bfdc378ce0
mariolino007 <69421768+mariolino007@users.noreply.github.com>
2025-03-30 18:12:38 +0200

    Update python-G2lib.cc

--- src_py/src/python-G2lib.cc
+++ src_py/src/python-G2lib.cc
@@ -7,6 +7,7 @@
  */
 
 #include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
 #include "python-BaseCurve.hh"
 #include "python-LineSegment.hh"
 #include "python-CircleArc.hh"
@@ -15,41 +16,61 @@
 #include "python-Biarc.hh"
 #include "python-Clothoid.hh"
 
-
 namespace py = pybind11;
 using namespace G2lib;
 using namespace G2lib::python;
 
-PYBIND11_MODULE(G2lib, m) {
-  wrap_BaseCurve(m);
-  wrap_AABBtree(m);
-  wrap_Triangle2D(m);
-  wrap_LineSegment(m);
-  wrap_CircleArc(m);
-  wrap_Biarc(m);
-  wrap_ClothoidCurve(m);
-  wrap_PolyLine(m);
-  wrap_BiarcList(m);
-  wrap_ClothoidList(m);
-  wrap_ClothoidSplineG2(m);
+// Aggiunta esplicita dell'export per Windows
+#ifdef _MSC_VER
+    #define EXPORT_SYMBOL __declspec(dllexport)
+#else
+    #define EXPORT_SYMBOL
+#endif
 
-  m.attr("__version__") = py::str("2.0.9");
+EXPORT_SYMBOL PYBIND11_MODULE(G2lib, m) {
+    // Wrap delle classi principali
+    wrap_BaseCurve(m);
+    wrap_AABBtree(m);
+    wrap_Triangle2D(m);
+    wrap_LineSegment(m);
+    wrap_CircleArc(m);
+    wrap_Biarc(m);
+    wrap_ClothoidCurve(m);
+    wrap_PolyLine(m);
+    wrap_BiarcList(m);
+    wrap_ClothoidList(m);
+    wrap_ClothoidSplineG2(m);
 
-  py::class_<Solve2x2>(m, "Solve2x2")
-    .def(py::init())
-    .def("factorize", [](Solve2x2 * self, std::tuple<std::tuple<real_type, real_type>, std::tuple<real_type, real_type>> A) {
-      real_type A_[2][2];
-      A_[0][0] = std::get<0>(std::get<0>(A));
-      A_[1][0] = std::get<0>(std::get<1>(A));
-      A_[0][1] = std::get<1>(std::get<0>(A));
-      A_[1][1] = std::get<1>(std::get<1>(A));
-      return self->factorize(A_);
-    })
-    .def("solve", [](Solve2x2 * self, std::tuple<real_type, real_type> b) {
-      real_type b_[2], x_[2];
-      b_[0] = std::get<0>(b);
-      b_[1] = std::get<1>(b);
-      self->solve(b_, x_);
-      return std::make_tuple(x_[0], x_[1]);
-    });
+    // Versione del modulo
+    m.attr("__version__") = py::str("2.0.9");
+
+    // Classe per risoluzione sistema lineare 2x2
+    py::class_<Solve2x2>(m, "Solve2x2")
+        .def(py::init())
+        .def("factorize", 
+            [](Solve2x2* self, 
+               std::tuple<std::tuple<real_type, real_type>, 
+                          std::tuple<real_type, real_type>> A) {
+                real_type A_[2][2];
+                A_[0][0] = std::get<0>(std::get<0>(A));
+                A_[1][0] = std::get<0>(std::get<1>(A));
+                A_[0][1] = std::get<1>(std::get<0>(A));
+                A_[1][1] = std::get<1>(std::get<1>(A));
+                return self->factorize(A_);
+            })
+        .def("solve", 
+            [](Solve2x2* self, std::tuple<real_type, real_type> b) {
+                real_type b_[2], x_[2];
+                b_[0] = std::get<0>(b);
+                b_[1] = std::get<1>(b);
+                self->solve(b_, x_);
+                return std::make_tuple(x_[0], x_[1]);
+            });
+
+    // Aggiunta esplicita dell'inizializzatore per MSVC
+    #ifdef _MSC_VER
+        m.def("__init_module__", []() { 
+            /* Inizializzazione aggiuntiva se necessaria */ 
+        });
+    #endif
 }
